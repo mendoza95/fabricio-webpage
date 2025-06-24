733e09080c8113a7c773bfdda54087330efabca0
Fabricio Mendoza <fabromendoza95@gmail.com>
2025-06-24 15:01:50 +0100

    We changed the structure, add more style and a version in Spanish

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
+# Default route redirects to English
 @app.route('/')
-def home():
-    return render_template('index.html')
+def default():
+    return redirect('/en/')
+
+@app.route('/<lang>/')
+def index(lang):
+    return render_template(f'{lang}/index.html', lang=lang)
+
+@app.route('/<lang>/about')
+def about(lang):
+    return render_template(f'{lang}/about.html', lang=lang)
+
+@app.route('/<lang>/projects')
+def projects(lang):
+    return render_template(f'{lang}/projects.html', lang=lang)
+
+@app.route('/<lang>/education')
+def education(lang):
+    return render_template(f'{lang}/education.html', lang=lang)
 
 if __name__ == '__main__':
     app.run(debug=True)
