4b4a38f3636f795bdc31f55dd960057004ba99e0
Fabricio Mendoza <fabromendoza95@gmail.com>
2026-09-16 16:09:22 -0500

    We added tests for post and delete operations for projects and portfolio global - the delete test for portfolio global raise an error

--- backend/tests/test_crud.py
+++ backend/tests/test_crud.py
@@ -1,8 +1,20 @@
 import pytest
 
 # -----------------------------------------------------------------------------
-# 1. Pruebas para Portfolio Global (PUT)
+# 1. Pruebas para Portfolio Global (GET, PUT)
 # -----------------------------------------------------------------------------
+
+@pytest.mark.asyncio
+async def test_create_portfolio_global(client, 
+                                                auth_headers,
+                                                sample_portfolio_global_payload):
+    """Verifica que un usuario no auntenticado no pueda leer la los skills"""
+
+    post_response = await client.post("/api/v1/portfolio/global",
+                                    json=sample_portfolio_global_payload,
+                                    headers=auth_headers)
+
+    assert post_response.status_code == 201
 
 @pytest.mark.asyncio
 async def test_update_portfolio_global_unauthorized(client):
@@ -30,13 +42,36 @@ async def test_update_portfolio_global_success(client, auth_headers):
     assert data["skills"][0]["en"] == "FastAPI Master"
     assert len(data["social_media"]) == 1
 
+@pytest.mark.asyncio
+async def test_delete_portfolio_global(client,
+                                       sample_portfolio_global_payload, 
+                                       auth_headers):
+    """Elimina la lista de skills y social media links del portfolio global"""
+    create_response = await client.post(
+        "/api/v1/portfolio/global",
+        json=sample_portfolio_global_payload,
+        headers=auth_headers
+    )
+
+    assert create_response.status_code == 201
+
+    print(create_response.json())
+    global_id = create_response.json().get("id")
+    print(global_id)
+
+    delete_response = await client.delete(
+        "/api/v1/portfolio/global/{global_id}",
+        headers=auth_headers
+    )
+
+    assert delete_response.status_code == 204
 
 # -----------------------------------------------------------------------------
 # 2. Pruebas para Projects (POST / GET / DELETE)
 # -----------------------------------------------------------------------------
 
 @pytest.mark.asyncio
-async def test_create_and_read_project(client, auth_headers, sample_project_payload):
+async def test_create_project(client, auth_headers, sample_project_payload):
     """Prueba la creación de un nuevo proyecto y su posterior consulta."""
 
     # 1. Crear el proyecto (Protegido)
@@ -49,13 +84,6 @@ async def test_create_and_read_project(client, auth_headers, sample_project_payl
     created_data = create_response.json()
     assert created_data["translations"]["es"]["name"] == "Portafolio Personal"
     assert "id" in created_data or "_id" in created_data
-
-    # 2. Consultar la lista pública de proyectos
-    get_response = await client.get("/api/v1/portfolio/projects")
-    assert get_response.status_code == 200
-    projects_list = get_response.json()
-    assert len(projects_list) >= 1
-
 
 @pytest.mark.asyncio
 async def test_create_project_invalid_payload(client, auth_headers):
