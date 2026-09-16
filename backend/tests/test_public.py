53338c16d4d26cee5bf32a961d890815e143da43
Fabricio Mendoza <fabromendoza95@gmail.com>
2026-09-16 16:09:59 -0500

    We added test for get operations on projects and portfolio global

--- backend/tests/test_public.py
+++ backend/tests/test_public.py
@@ -10,16 +10,32 @@ async def test_get_ui_translations(client):
     assert "en" in data["translations"]
 
 @pytest.mark.asyncio
-async def test_get_portfolio_global_authenticated(client, auth_headers):
-    """Endpoint protegido de configuración global."""
-    response = await client.get("/api/v1/portfolio/global", headers=auth_headers)
+async def test_get_portfolio_global(client):
+    """Endpoint de configuración global."""
+    response = await client.get("/api/v1/portfolio/global")
     assert response.status_code == 200
     data = response.json()
     assert "skills" in data
     assert "social_media" in data
 
 @pytest.mark.asyncio
-async def test_get_portfolio_global_unauthorized(client):
-    """Acceso denegado a configuración global sin token JWT."""
-    response = await client.get("/api/v1/portfolio/global")
-    assert response.status_code == 401
+async def test_get_projects(client):
+    """Endpoint de proyectos"""
+    response = await client.get("/api/v1/portfolio/projects")
+    assert response.status_code == 200
+
+    data = response.json()[0] #retrieve the only project element
+
+    assert "project_id" in data
+    assert data["translations"]["en"]["name"] == "Personal Portfolio"
+
+@pytest.mark.asyncio
+async def test_get_experience(client):
+    """Endpoint de Experience"""
+    response = await client.get("/api/v1/portfolio/experience")
+    assert response.status_code == 200
+
+    data = response.json()[0] #retrieve the only experience element
+
+    assert "experience_id" in data
+    assert "role" in data["translations"]["en"]
