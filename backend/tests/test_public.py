5aa5f7c914f06f83ef3abc8de7a100fa82d2794c
Fabricio Mendoza <fabromendoza95@gmail.com>
2026-09-17 17:55:28 -0500

    We finished testing all GET operations

--- backend/tests/test_public.py
+++ backend/tests/test_public.py
@@ -1,24 +1,6 @@
 import pytest
 
 @pytest.mark.asyncio
-async def test_get_ui_translations(client):
-    response = await client.get("/api/v1/portfolio/ui-translations")
-    assert response.status_code == 200
-    data = response.json()
-    assert "translations" in data
-    assert "es" in data["translations"]
-    assert "en" in data["translations"]
-
-@pytest.mark.asyncio
-async def test_get_portfolio_global(client):
-    """Endpoint de configuración global."""
-    response = await client.get("/api/v1/portfolio/global")
-    assert response.status_code == 200
-    data = response.json()
-    assert "skills" in data
-    assert "social_media" in data
-
-@pytest.mark.asyncio
 async def test_get_projects(client):
     """Endpoint de proyectos"""
     response = await client.get("/api/v1/portfolio/projects")
@@ -39,3 +21,43 @@ async def test_get_experience(client):
 
     assert "experience_id" in data
     assert "role" in data["translations"]["en"]
+
+@pytest.mark.asyncio
+async def test_get_education(client):
+    """Test public Education endpoint"""
+    response = await client.get("/api/v1/portfolio/education")
+    assert response.status_code == 200
+
+    data = response.json()[0] #retrieve the only education element
+
+    assert "education_id" in data
+    assert "degree" in data["translations"]["en"]
+
+@pytest.mark.asyncio
+async def test_get_publications(client):
+    """Test public Publications endpoint"""
+
+    response = await client.get("/api/v1/portfolio/publications")
+    assert response.status_code == 200
+
+    data = response.json()[0]
+    assert "publication_id" in data
+    assert "journal" in data["translations"]["en"]
+
+@pytest.mark.asyncio
+async def test_get_portfolio_global(client):
+    """Endpoint de configuración global."""
+    response = await client.get("/api/v1/portfolio/global")
+    assert response.status_code == 200
+    data = response.json()
+    assert "skills" in data
+    assert "social_media" in data
+
+@pytest.mark.asyncio
+async def test_get_ui_translations(client):
+    response = await client.get("/api/v1/portfolio/ui-translations")
+    assert response.status_code == 200
+    data = response.json()
+    assert "translations" in data
+    assert "es" in data["translations"]
+    assert "en" in data["translations"]
